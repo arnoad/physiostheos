cd50646803c1a312caa47aeb2d1673cdeea0d135
arno arend <arnoarend@icloud.com>
2023-07-11 18:37:25 +0200

    create controllers

--- db/migrate/20230711160349_add_column_to_users.rb
+++ db/migrate/20230711160349_add_column_to_users.rb
@@ -3,6 +3,5 @@ class AddColumnToUsers < ActiveRecord::Migration[7.0]
     add_column :users, :firstname, :string
     add_column :users, :lastnname, :string
     add_column :users, :phone, :string
-    # add_column :users, :email, :string
   end
 end
