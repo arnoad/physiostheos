70bfa50b67b98a0149054fd66958f55671732c05
arno arend <arnoarend@icloud.com>
2023-07-15 14:19:37 +0200

    create banner homepage - create links to homepage and physio index

--- app/controllers/physios_controller.rb
+++ app/controllers/physios_controller.rb
@@ -1,2 +1,16 @@
 class PhysiosController < ApplicationController
+
+  before_action :find_physio, only: %i[show]
+
+  def index
+    @physios = Physio.all
+  end
+
+  def show; end
+
+  private
+
+  def find_physio
+    @physio = Physio.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,23 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+puts "Cleaning DB"
+Physio.destroy_all
+
+puts "Create Physios"
 cedric = Physio.new(lastname: "Arend", firstname: "Cedric", specialty: "Kinésitherapeuthe - Thérapeute manuel - Osthéo- Éthiopathe", email: "cedric@physiostheos.com", phone: "+32 472 18 14 64", bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 cedric.save
-
-pascal = Physio.new(lastname: "Vigneron", firstname: "Pascal", specialty: "Kinésitherapeuthe - Thérapeute manuel - Osthéo- Éthiopathe", email: "pascal@physiostheos.com", phone: "+32 484 14 49 11", bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-pascal.save
-
-job = Physio.new(lastname: "Mabanga", firstname: "Job", specialty: "Kinésitherapeuthe - Thérapeute manuel - Osthéo- Éthiopathe", email: "job@physiostheos.com", phone: "+32 491 97 79 22", bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-job.save
 
 rinor = Physio.new(lastname: "Aziri", firstname: "Rinor", specialty: "Kinésitherapeuthe - Thérapeute manuel - Osthéo- Éthiopathe", email: "rinor@physiostheos.com", phone: "+32 489 36 96 68", bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 rinor.save
 
 murielle = Physio.new(lastname: "Bully", firstname: "Murielle", specialty: "Kinésitherapeuthe spécialisée en uro-gynécologie - Sexologue", email: "murielle@physiostheos.com", phone: "+32 474 41 93 72", bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 murielle.save
+
+job = Physio.new(lastname: "Mabanga", firstname: "Job", specialty: "Kinésitherapeuthe - Thérapeute manuel - Osthéo- Éthiopathe", email: "job@physiostheos.com", phone: "+32 491 97 79 22", bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+job.save
+
+pascal = Physio.new(lastname: "Vigneron", firstname: "Pascal", specialty: "Kinésitherapeuthe - Thérapeute manuel - Osthéo- Éthiopathe", email: "pascal@physiostheos.com", phone: "+32 484 14 49 11", bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+pascal.save
+
+puts "DONE"
